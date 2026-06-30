135475a7b115da0dfa565d449294964defb33b22
rafaellrossii <rafael.rossij@gmail.com>
2026-06-30 00:28:23 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,29 @@
-// import fs from 'fs';
-
-// import { launch } from '@cloudflare/playwright';
-// import { expect } from '@cloudflare/playwright/test';
-
-// const CORS_HEADERS = {
-//   'Access-Control-Allow-Origin': 'https://trace.playwright.dev',
-//   'Access-Control-Allow-Methods': 'GET',
-//   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-// };
-
-// export default {
-//   async fetch(request: Request, env: Env) {
-//     const url = new URL(request.url);
-    
-//     if (url.pathname !== '/') {
-//       return new Response(null, { status: 404 });
-//     }
-
-//     if (request.method === 'OPTIONS') {
-//       return new Response(null, { headers: CORS_HEADERS });
-//     }
-    
-//     const trace = url.searchParams.has('trace') || request.headers.get('referer') === 'https://trace.playwright.dev/';
-//     const todos = url.searchParams.getAll('todo');    
-    
-//     const browser = await launch(env.MYBROWSER);
-//     const page = await browser.newPage();
-    
-//     // if (trace)
-//     //   await page.context().tracing.start({ screenshots: true, snapshots: true });
-
-//     await page.goto('https://vmbro-lt-75.pages.dev/');
-
-//     await new Promise((r) => setTimeout(r, 50000000));
-
-
-//     // const TODO_ITEMS = todos.length > 0 ? todos : [
-//     //   'buy some cheese',
-//     //   'feed the cat',
-//     //   'book a doctors appointment'
-//     // ];
-
-//     // const newTodo = page.getByPlaceholder('What needs to be done?');
-//     // for (const item of TODO_ITEMS) {
-//     //   await newTodo.fill(item);
-//     //   await newTodo.press('Enter');
-//     // }
-
-//     // await expect(page.getByTestId('todo-title')).toHaveCount(TODO_ITEMS.length);
-
-//     // await Promise.all(TODO_ITEMS.map(
-//     //     (value, index) => expect(page.getByTestId('todo-title').nth(index)).toHaveText(value)
-//     // ));
-
-//     // if (trace) {
-//     //   // we must write the trace to /tmp as it is the only directory 
-//     //   // that is writable in the worker
-//     //   //await page.context().tracing.stop({ path: '/tmp/trace.zip' });
-//     //   // await browser.close();
-//     //   const file = await fs.promises.readFile('/tmp/trace.zip');
-
-//     //   return new Response(new Uint8Array(file), {
-//     //     status: 200,
-//     //     headers: {
-//     //       'Content-Type': 'application/zip',
-//     //       ...CORS_HEADERS,
-//     //     },
-//     //   });
-//     // } else {
-//       const img = await page.screenshot();
-//       // await browser.close();
-
-//       return new Response(new Uint8Array(img), {
-//         headers: {
-//           'Content-Type': 'image/png',
-//         },
-//       });
-//     // }
-//   },
-// };
-
-
 import { launch } from "@cloudflare/playwright";
-import { expect } from "@cloudflare/playwright/test";
 
 export default {
   async fetch(request, env): Promise<Response> {
     const browser = await launch(env.MYBROWSER);
-    const page = await browser.newPage();
 
-    // await page.goto("https://demo.playwright.dev/todomvc");
+    try {
+      const page = await browser.newPage();
 
-    await page.goto('https://vmbro-lt-75.pages.dev/');
+      await page.goto("https://vmbro-lt-75.pages.dev/");
 
-    // await new Promise((r) => setTimeout(r, 50000000));
+      await new Promise((r) => setTimeout(r, 500000));
 
-    // const TODO_ITEMS = [
-    //   "buy some cheese",
-    //   "feed the cat",
-    //   "book a doctors appointment",
-    // ];
 
-    // const newTodo = page.getByPlaceholder("What needs to be done?");
-    // for (const item of TODO_ITEMS) {
-    //   await newTodo.fill(item);
-    //   await newTodo.press("Enter");
-    // }
+      const image = await page.screenshot({
+        type: "png",
+        fullPage: true,
+      });
 
-    // await expect(page.getByTestId("todo-title")).toHaveCount(TODO_ITEMS.length);
-
-    // await Promise.all(
-    //   TODO_ITEMS.map((value, index) =>
-    //     expect(page.getByTestId("todo-title").nth(index)).toHaveText(value)
-    //   )
-    // );
-  }
+      return new Response(image, {
+        headers: {
+          "Content-Type": "image/png",
+        },
+      });
+    } finally {
+      await browser.close();
+    }
+  },
 } satisfies ExportedHandler<Env>;
